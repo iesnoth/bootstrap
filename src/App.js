14ce023a4bfb3e77b7c29a2415889906762edcef
Victoria Beasley <iesnoth91@gmail.com>
2022-11-23 20:28:04 -0600

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg='warning' expand='lg'>
+        <Nav activeKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="/about">About Us</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
+      <h1>Bootstrap Website</h1>
+      <div>
+        <Button variant="outline-info">Hello</Button>
+      </div>
+
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Form Dropdown with favorite movies</Form.Label>
+          <Form.Control as="select">
+            <option>Inception</option>
+            <option>Forrest Gump</option>
+            <option>Shawshank Redemption</option>
+            <option>Spiderman: Into the Spiderverse</option>
+            <option>Avengers: Endgame</option>
+          </Form.Control>
+        </Form.Group>
+
+
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
     </div>
   );
 }
